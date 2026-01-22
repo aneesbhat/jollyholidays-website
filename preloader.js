045ccd921587f2b47ff235a845b19a6f1665a1a0
Anees Bhat <bisma1055@gmail.com>
2026-01-22 14:11:22 +0530

    a

--- preloader.js
+++ preloader.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", () => {
     const loader = document.getElementById("jh-preloader");
     loader.classList.add("hide");
-    setTimeout(() => loader.remove(), 500);
+    setTimeout(() => loader.remove(), 200);
   });
