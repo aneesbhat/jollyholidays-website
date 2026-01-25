3e1e9952b6c90b4ee87ea1dcc3ae59239de47cec
Anees Bhat <bisma1055@gmail.com>
2026-01-25 14:32:07 +0530

    changes in kashmir package

--- offcanvas.js
+++ offcanvas.js
@@ -1,3 +1,19 @@
-const offcanvas = document.getElementById("offcanvas");
-document.getElementById("openCanvasBtn").addEventListener("click", () => offcanvas.classList.add("active"));
-document.getElementById("closeCanvasBtn").addEventListener("click", () => offcanvas.classList.remove("active"));
+const hamburger=document.getElementById("hamburger");
+const offcanvas=document.getElementById("offcanvas");
+const overlay=document.getElementById("overlay");
+const closeBtn=document.getElementById("close");
+const toggle=document.querySelector(".off-toggle");
+
+hamburger.onclick=()=>{
+  offcanvas.classList.add("active");
+  overlay.classList.add("active");
+};
+
+overlay.onclick=closeBtn.onclick=()=>{
+  offcanvas.classList.remove("active");
+  overlay.classList.remove("active");
+};
+
+toggle.onclick=function(){
+  this.parentElement.classList.toggle("active");
+};
